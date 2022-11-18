86dd82d0dd57735160cdbba77597f7d11a78afdc
Han Jewoo <zkspf1@gmail.com>
2022-11-18 22:09:14 +0900

    11/18

--- Main/App.js
+++ Main/App.js
@@ -4,21 +4,21 @@
 // Import React and Component
 import React from 'react';
 // Import Navigators from React Navigation
-import {NavigationContainer, useRoute} from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 //임포트할 화면들/
-import LoginMain from './Screens/Login/LoginMain';
 import LoginBus from './Screens/Login/LoginBus';
 import LoginStudent from './Screens/Login/LoginStudent';
-import BusLocation from './Screens/ScreenMain/BusLocation';
+import BusMain from './Screens/ScreenMain/BusMain';
 import HomeMain from './Screens/ScreenMain/HomeMain';
 import Busr from './Screens/ScreenMain/Busr';
 import BusMap from './Screens/ScreenMain/BusMap';
+import Setting from './Screens/ScreenMain/Setting';
+import JoinMembership from './Screens/Login/JoinMembership';
 //임포트 추가
-import Routes from './Screens/ScreenMain/Routes';
+// import Routes from './Screens/ScreenMain/Routes';
 import QRCodeScannerScreen from "./Screens/ScreenMain/QRCodeScannerScreen";
-import QRCodeData from "./Screens/ScreenMain/QRCodeData";
 
 const Stack = createNativeStackNavigator();
 
@@ -50,14 +50,9 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="LoginMain">
+      <Stack.Navigator initialRouteName="LoginStudent">
         {/* SplashScreen which will come once for 5 Seconds */}
         {/* Auth Navigator: Include Login and Signup */}
-        <Stack.Screen
-          name="LoginMain"
-          component={LoginMain}
-          options={{headerShown: false}}
-        />
         <Stack.Screen
           name="LoginBus"
           component={LoginBus}
@@ -83,29 +78,28 @@ const App = () => {
           component={HomeMain}
           options={{headerShown: false}}
         />
-
+         <Stack.Screen
+          name="BusMain"
+          component={BusMain}
+          options={{headerShown: false}}
+        />
         <Stack.Screen
-          name="BusLocation"
-          component={BusLocation}
+          name="Setting"
+          component={Setting}
           options={{headerShown: false}}
         />
-          <Stack.Screen
-          name="Routes"
-          component={Routes}
-          options={{headerShown: false}}
-        />
-
         <Stack.Screen
           name="QRCodeScannerScreen"
           component={QRCodeScannerScreen}
           options={{headerShown: false}}
         />
-        <Stack.Screen
-          name="QRCodeData"
-          component={QRCodeData}
+
+<Stack.Screen
+          name="JoinMembership"
+          component={JoinMembership}
           options={{headerShown: false}}
         />
-        
+
         {/* Navigation Drawer as a landing page */}
       </Stack.Navigator>
     </NavigationContainer>
