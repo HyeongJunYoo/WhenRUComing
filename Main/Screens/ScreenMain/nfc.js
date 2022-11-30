472f2137a8528dea63045658b0e9192ef4fe6c38
홍민재 <42972380+98HMJ@users.noreply.github.com>
2022-11-30 20:02:32 +0900

    Add files via upload

---
+++ Main/Screens/ScreenMain/nfc.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import {
+  View, Text, TouchableOpacity
+} from 'react-native'
+import NfcManager, {NfcEvents} from 'react-native-nfc-manager';
+import firestore from '@react-native-firebase/firestore';
+
+const addCollection = firestore().collection('bus');
+
+class AppV2 extends React.Component {
+  componentDidMount() {
+    NfcManager.start();
+    NfcManager.setEventListener(NfcEvents.DiscoverTag, tag => {
+      console.warn('tag', tag);
+      console.log(tag);
+      console.log(tag.id)
+      if(tag.id=="043D97EAC07480"){
+        addCollection.doc("1234").get().then((doc)=>{
+          if(doc.data().student_NUM<45){
+            const number=doc.data().student_NUM;
+            addCollection.doc("1234").update({student_NUM:number+1});
+          }
+        })
+      }
+      NfcManager.unregisterTagEvent().catch(() => 0);
+    });
+  }
+
+  componentWillUnmount() {
+    NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
+    NfcManager.unregisterTagEvent().catch(() => 0);
+  }
+
+  render() {
+    return (
+      <View style={{padding: 20}}>
+        <Text>NFC Demo</Text>
+        <TouchableOpacity 
+          style={{padding: 10, width: 200, margin: 20, borderWidth: 1, borderColor: 'black'}}
+          onPress={this._test}
+        >
+          <Text>Test</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity 
+          style={{padding: 10, width: 200, margin: 20, borderWidth: 1, borderColor: 'black'}}
+          onPress={this._cancel}
+        >
+          <Text>Cancel Test</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+  _cancel = () => {
+    NfcManager.unregisterTagEvent().catch(() => 0);
+  }
+
+  _test = async () => {
+    try {
+      await NfcManager.registerTagEvent();
+    } catch (ex) {
+      console.warn('ex', ex);
+      NfcManager.unregisterTagEvent().catch(() => 0);
+    }
+  }
+}
+
+export default AppV2
